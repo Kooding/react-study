46f488f3087fbf130b71af40d386d986335a3628
Kooding <giyun529@hanmail.net>
2020-07-21 13:32:48 +0900

    03-6 정리

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
-import MyComponenet from "./03-6-Class-Components/MyComponenet";
-import PureComponent from "./03-6-Class-Components/PureComponent";
+import MyComponenet from "./03-6-PureComponents/MyComponenet";
+import PureComponent from "./03-6-PureComponents/PureComponent";
 
 function App() {
   return (
